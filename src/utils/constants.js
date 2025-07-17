1b5ccd95a615b108aa121d953cfc2029c55af08c
hungnguyen16112003 <nguyenquochung16112003@gmail.com>
2025-07-18 00:32:14 +0700

    update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ let API_URL = "";
 
 if (import.meta.env.DEV) {
   //   API_URL = "https://localhost:3001";
-  API_URL = "http://localhost:3001";
+  API_URL = "http://160.250.180.19";
 }
 
 if (import.meta.env.PROD) {
