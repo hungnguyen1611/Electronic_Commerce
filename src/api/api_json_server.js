0d3c30629eb516613694137015b7a1cb4e31bea7
hungnguyen16112003 <nguyenquochung16112003@gmail.com>
2025-07-18 01:05:43 +0700

    update_api_url

--- src/api/api_json_server.js
+++ src/api/api_json_server.js
@@ -1,11 +1,11 @@
 import axiosInstance from "../utils/axiosInstance";
 
 export const fetchProducts = async (searchPath) => {
-  const res = await axiosInstance.get(`/products${searchPath}`);
+  const res = await axiosInstance.get(`/api/products${searchPath}`);
   return res.data;
 };
 
 export const fetchProduct = async (id) => {
-  const res = await axiosInstance.get(`/products/${id}`);
+  const res = await axiosInstance.get(`/api/products/${id}`);
   return res.data;
 };
